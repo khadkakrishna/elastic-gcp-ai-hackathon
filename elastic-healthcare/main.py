98b885a872c6f24228783b682451de325ec9c0cd
krishna khadka <krishnakhadka2802@gmail.com>
2025-10-22 14:34:57 +0530

    rename vector-search script

--- elastic-healthcare/main.py
+++ elastic-healthcare/main.py
@@ -1,27 +1,31 @@
 import streamlit as st
+import sidebar
+import utils
 
-st.title("Hello World! this is beginning of great things 👋🌎")
+sidebar.sidebar()
+
+st.title("🩺 Elastic Healthcare")
 st.markdown(
    """
-   This is a demo Streamlit app.
+   Smarter Insights for Better Care.
    Enter your name in the text box below and press a button to see some fun features in Streamlit.
    """
 )
 
 name = st.text_input("Enter your name please:")
 
+
 # Use columns to create buttons side by side
 col1, col2 = st.columns(2)
 
+ans = ""
 with col1:
-   if st.button("Send balloons! 🎈"):
-      st.balloons()
-      st.write(f"Time to celebrate {name}! 🥳")
-      st.write("You deployed a Streamlit app! 👏")
+   if st.button("Get embeddings!"):
+      ans = utils.get_embedding(name)
 
 with col2:
-   if st.button("Send snow! ❄️"):
-      st.snow()
-      st.write(f"Let it snow {name}! 🌨️")
-      st.write("You deployed a Streamlit app! 👏")
-
+   if st.button("Get answers from Elasticsearch!"):
+      
+      ans = utils.get_answers_from_elasticsearch(name)
+st.write(f"{name}")
+st.write(ans)

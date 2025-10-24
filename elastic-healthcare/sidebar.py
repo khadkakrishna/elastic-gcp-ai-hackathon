91680f81dc8095b6a7778c8acb5857ef9d67e547
krishna khadka <krishnakhadka2802@gmail.com>
2025-10-24 21:09:04 +0530

    Refactor sidebar

--- elastic-healthcare/sidebar.py
+++ elastic-healthcare/sidebar.py
@@ -6,10 +6,6 @@ def sidebar():
             image="assets/main.png",
         )
 
-        st.markdown("### About")
-        st.markdown("""
-        Elastic HealthCare is an AI-powered healthcare intelligence platform that combines Google Cloud’s Vertex AI and Elasticsearch to deliver real-time, multimodal, and multilingual insights from structured and unstructured medical data.
-        """)
         st.markdown("---")
         st.caption("Built with ❤️ using Elasticsearch, GCP(Vertex AI and Gemini), and Streamlit.")
         st.caption("© 2025 Krishna Khadka | krishnakhadka2802@gmail.com")
